5b4906d61523c62abb3532ba58b770351e44592a
Delveri Munang <chickdelveri@gmail.com>
2017-05-25 12:20:54 -0400

    Fixed Issue #36: Added Drop down for past Events

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,27 +3,35 @@
 */
 
 $(window).scroll(function(){
-	handleTopNavAnimation();
+    handleTopNavAnimation();
 });
 
 $(window).load(function(){
-	handleTopNavAnimation();
+    handleTopNavAnimation();
 });
 
 function handleTopNavAnimation() {
-	var top=$(window).scrollTop();
+    var top=$(window).scrollTop();
 
-	if(top>10){
-		$('#site-nav').addClass('navbar-solid'); 
-	}
-	else{
-		$('#site-nav').removeClass('navbar-solid'); 
-	}
+    if(top>10){
+        $('#site-nav').addClass('navbar-solid'); 
+    }
+    else{
+        $('#site-nav').removeClass('navbar-solid'); 
+    }
 }
 
 /*
  * Registration Form
 */
+
+$('#dropdown').hide();
+
+$('#pst').hover(function(){
+    $('#dropdown').show();
+}, function(){
+    $('#dropdown').hide();
+});
 
 $('#registration-form').submit(function(e){
     e.preventDefault();
